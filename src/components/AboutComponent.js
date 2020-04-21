592e65d68e3122f883ff0d4ac0179ce95706aaf8
Ashwini Jadhav <ashwinijadhav925@gmail.com>
2020-04-21 19:16:07 +0530

    Controlled Forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, CardHeader, Media , CardImg, CardText, CardTitle , CardImgOverlay,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import { Card, CardBody, CardHeader, Media , CardImg, CardText, CardTitle , CardImgOverlay,Breadcrumb,BreadcrumbItem, Col} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function RenderLeader ({leader}) {
